f86bb3515884b56a0cb8c7a161db1ae4cb2de72f
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2016-05-19 06:16:18 -0400

    updated for external

--- scripts/startUnisonInDocker.sh
+++ scripts/startUnisonInDocker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
-export TREMOLO_ROOT="/usr/local/tremolo"
-export TREMOLO_HOME="$TREMOLO_ROOT/tremolo-service"
-export TREMOLO_PROXY_HOME="$TREMOLO_HOME/external/apps/proxy"
-export TREMOLO_ADMIN_HOME="$TREMOLO_HOME/external/apps/tremolo-admin"
-export TREMOLO_WS_HOME="$TREMOLO_HOME/external/apps/webservices"
+export TREMOLO_ROOT="/usr/local/tremolo/tremolo-service"
+export TREMOLO_HOME="$TREMOLO_ROOT/external"
+export TREMOLO_PROXY_HOME="$TREMOLO_HOME/apps/proxy"
+export TREMOLO_ADMIN_HOME="$TREMOLO_HOME/apps/tremolo-admin"
+export TREMOLO_WS_HOME="$TREMOLO_HOME/apps/webservices"
 export TREMOLO_SSH_KEYS="$TREMOLO_ROOT/.ssh"
-export TREMOLO_QUARTZ_DIR="$TREMOLO_HOME/external/conf/quartz"
+export TREMOLO_QUARTZ_DIR="$TREMOLO_HOME/conf/quartz"
 export TREMOLO_LOG="$TREMOLO_HOME/logs/tremolo.log"
 export TREMOLO_ACTIVEMQ="$TREMOLO_HOME/activemq"
-export TREMOLO_EXTERNAL="$TREMOLO_HOME/external/ext-lib"
+export TREMOLO_EXTERNAL="$TREMOLO_HOME/ext-lib"
 
 
 JAVA_CMD=`which java`
@@ -19,11 +19,13 @@ if [ ! $JAVA_CMD ]; then
   exit 1
 fi
 
-export CLASSPATH=$CLASSPATH:$TREMOLO_HOME/libs/*:$TREMOLO_HOME/jars/*:$TREMOLO_HOME/external/ext-lib/*:$TREMOLO_QUARTZ_DIR
+export CLASSPATH=$CLASSPATH:$TREMOLO_ROOT/libs/*:$TREMOLO_ROOT/jars/*:$TREMOLO_HOME/ext-lib/*:$TREMOLO_QUARTZ_DIR
 
 TREMOLO_CMD="$JAVA_CMD -Dorg.eclipse.jetty.server.Request.maxFormContentSize=-1 -Dcom.tremolosecurity.openunison.activemqdir=$TREMOLO_ACTIVEMQ -Dcom.tremolosecurity.openunison.quartzdir=$TREMOLO_QUARTZ_DIR $JAVA_OPTS -server com.tremolosecurity.server.Server"
 
 echo "Starting Xvfb..."
+
+rf -f /tmp/.X0-lock
 
 #Start Xvfb
 /usr/bin/Xvfb :0.0 >& /dev/null &
